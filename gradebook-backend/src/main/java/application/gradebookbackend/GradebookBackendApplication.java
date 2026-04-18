42c60d381d20f6c528e0e9237300a2f6c73dab67
Kaloyan <kaloyan.ivanov@one16.be>
2026-04-18 17:20:03 +0200

    initial project repo setup

---
+++ gradebook-backend/src/main/java/application/gradebookbackend/GradebookBackendApplication.java
@@ -0,0 +1,13 @@
+package application.gradebookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradebookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradebookBackendApplication.class, args);
+    }
+
+}
---
+++ gradebook-backend/src/test/java/application/gradebookbackend/GradebookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package application.gradebookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradebookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
